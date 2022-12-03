afb513b33e136f994c72589771e7ad7165ec8a53
ddiu8081 <ddiu8081@gmail.com>
2022-12-03 22:51:39 +0800

    feat: pinyin ui

--- src/components/Char.tsx
+++ src/components/Char.tsx
@@ -1,5 +1,6 @@
-import type { Component } from 'solid-js'
+import type { Component, JSX } from 'solid-js'
 import type { CharCheck } from '../utils/check'
+import { getTonePosition, toneSvgPath } from '../utils/pinyin'
 
 interface Props {
   char: string
@@ -7,6 +8,11 @@ interface Props {
 }
 
 const palette = {
+  foregroundColor: [
+    '#999999',
+    '#e2c97e',
+    '#66b395',
+  ],
   foreground: [
     'text-[#999999]',
     'text-[#e2c97e]',
@@ -19,21 +25,51 @@ const palette = {
   ],
 }
 
+
 export const Char: Component<Props> = (props) => {
+  const detail = props.result.detail
+  const match = props.result.match
+  const tonePos = getTonePosition(detail.pinyin.yunmu)
+  const needReplaceI = tonePos === 0 && detail.pinyin.yunmu[0] === 'i'
+
+  const toneLeftOffset = needReplaceI ? -0.1 : tonePos * 0.64
+  const toneStyle: JSX.CSSProperties = {
+    position: 'absolute',
+    top: '-0.02em',
+    left: `${toneLeftOffset}em`,
+    width: '0.5em',
+    height: '0.5em',
+    color: palette.foregroundColor[match.pinyin.tone + 1],
+  }
+
   return (
     <div inline-flex flex-col items-center w-11>
-      <div mb-1 text-xs>
-        <span class={palette.foreground[props.result.match.pinyin.shengmu + 1]}>{ props.result.detail.pinyin.shengmu }</span>
-        <span class={palette.foreground[props.result.match.pinyin.yunmu + 1]}>{ props.result.detail.pinyin.yunmu }</span>
-        <span class={palette.foreground[props.result.match.pinyin.tone + 1]}>{ props.result.detail.pinyin.tone }</span>
+      <div mb-1 text-sm font-sans>
+        <span class={palette.foreground[match.pinyin.shengmu + 1]}>
+          { detail.pinyin.shengmu }
+        </span>
+        <span
+          class={palette.foreground[match.pinyin.yunmu + 1]}
+          relative
+        >
+          { needReplaceI ? detail.pinyin.yunmu.replace('i', 'ı') : detail.pinyin.yunmu }
+          <div style={toneStyle}>
+            <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="text-ok" absolute w="100%" left="0" top="0">
+              <path
+                d={toneSvgPath[detail.pinyin.tone - 1]}
+                fill="currentColor"
+              />
+            </svg>
+          </div>
+        </span>
       </div>
       <div
         flex items-center justify-center
         text="xl white"
         w-10 h-10
-        class={palette.background[props.result.match.char + 1]}
+        class={palette.background[match.char + 1]}
       >
-        { props.result.detail.char }
+        { detail.char }
       </div>
     </div>
   )
